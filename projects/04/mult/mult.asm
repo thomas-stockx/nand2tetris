dfb9e480c82e23dc001a8ff7cfd9aff244c1373d
Thomas Stockx <thomas@stockxit.com>
2017-06-01 21:06:09 +0200

    Implemented the multiplication program in Hack Assembly Language (Project 4)

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,53 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Implementation by Thomas Stockx
+
+// Pseudo code:
+// R2 = 0;
+// n = R1;
+// while (n > 0) {
+//  R2 = R2 + R0;
+//  n = n - 1;
+// }
+
+// Initialisation
+    // R2 = 0;
+    @R2
+    M=0 
+    // n = R1;
+    @R1
+    D=M
+    @n
+    M=D
+    
+// While loop
+(LOOP)
+    // Check while loop condition
+    // while (n > 0)
+    @n
+    D=M
+    // if i == 0 go to END
+    @END
+    D;JEQ
+    
+    // Do while loop internals
+    // R2 = R2 + R0;
+    @R0
+    D=M
+    @R2
+    M=D+M
+    // n = n - 1;
+    @n
+    M=M-1
+    
+    // jump to condition check of while loop
+    @LOOP
+    0;JMP
+
+    
+// Infinite loop to end
+(END)
+    @END
+    0;JMP
+    
